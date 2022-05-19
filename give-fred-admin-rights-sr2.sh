178a68351364cf3970c28fdf88f015bfec75d63d
Benedikt Linse <benedikt@confluent.io>
2022-05-19 06:44:53 +0200

    securing sr2; reconfiguring c3 for secured sr2

---
+++ give-fred-admin-rights-sr2.sh
@@ -0,0 +1,1 @@
+sr-acl-cli --config schema-registry2.properties --add -s '*' -p fred -o '*'
--- start-schema-registry-2.sh
+++ start-schema-registry-2.sh
@@ -1,1 +1,5 @@
+#!/bin/bash
+
+export SCHEMA_REGISTRY_OPTS=-Djava.security.auth.login.config=/Users/benedikt/repos/1123/datev/multiSRC3/jaas_config.conf
+
 schema-registry-start schema-registry2.properties > schema-registry2.log 2>&1 &
