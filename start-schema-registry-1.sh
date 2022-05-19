fba759fa43a99102c8b4212a97bac9bfe3312225
Benedikt Linse <benedikt@confluent.io>
2022-05-19 06:21:10 +0200

    enabling security plugin / password protection on schema-registry1

--- start-schema-registry-1.sh
+++ start-schema-registry-1.sh
@@ -1,1 +1,5 @@
+#!/bin/bash
+
+export SCHEMA_REGISTRY_OPTS=-Djava.security.auth.login.config=/Users/benedikt/repos/1123/datev/schema-registry-security/jaas_config.conf
+
 schema-registry-start schema-registry1.properties > schema-registry1.log 2>&1 &
