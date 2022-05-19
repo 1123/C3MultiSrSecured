c369483505c7690ffdbd57beb72ac32748b88c33
Benedikt Linse <benedikt@confluent.io>
2022-05-19 06:44:09 +0200

    bugfix

--- start-schema-registry-1.sh
+++ start-schema-registry-1.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-export SCHEMA_REGISTRY_OPTS=-Djava.security.auth.login.config=/Users/benedikt/repos/1123/datev/schema-registry-security/jaas_config.conf
+export SCHEMA_REGISTRY_OPTS=-Djava.security.auth.login.config=/Users/benedikt/repos/1123/datev/multiSRC3/jaas_config.conf
 
 schema-registry-start schema-registry1.properties > schema-registry1.log 2>&1 &
